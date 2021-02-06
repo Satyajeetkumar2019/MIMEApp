0dc6a96c318be83ab6829afc3501780a3ad4899c
satyajeet kumar <55208293+Satyajeetkumar2019@users.noreply.github.com>
2021-02-07 01:07:54 +0530

    Add files via upload

---
+++ XmlServlet.java
@@ -0,0 +1,26 @@
+package com.bce.servlet;
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+public class XmlServlet extends HttpServlet {
+public void service(HttpServletRequest req, HttpServletResponse res)throws ServletException ,IOException{
+//getprintWriter
+PrintWriter pw=res.getWriter();
+//set response
+res.setContentType("text/html");
+//Write logic to get out put 
+pw.println("<table border='1'>");
+pw.println("<tr><th>player</th> <th>Role</th> </tr>");
+pw.println("<tr><th>dhoni</th> <th>caption</th> </tr>");
+pw.println("<tr><th>sachin</th> <th>All Rounder</th> </tr>");
+pw.println("</table>");
+pw.close();
+}//end of the service(--)
+}//end classs
+
+
